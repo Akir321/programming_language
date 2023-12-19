f5bd4604002f8a21c678a5d3cea9141591d7b99b
Akir321 <anton-1945@bk.ru>
2023-12-19 16:31:40 +0300

    while to assembly

--- source/assembler_code.cpp
+++ source/assembler_code.cpp
@@ -72,12 +72,21 @@ int convertToAssemblyCode(Evaluator *eval, Node *root, FILE *f)
                                 fprintf(f, "\n");
                                 return EXIT_SUCCESS;
 
-        case EXP_TREE_OPERATOR: break;
+        case EXP_TREE_OPERATOR: return printAssemblyOperator(eval, root, f);
     
         default:                LOG("ERROR in %s(%d) in function %s: unknown Node type: %d\n",
                                     __FILE__, __LINE__ - 1, __func__, root->type);
                                 return EXIT_FAILURE;
     }
+
+    
+}
+
+int printAssemblyOperator(Evaluator *eval, Node *root, FILE *f)
+{
+    assert(eval);
+    assert(root);
+    assert(f);
 
     switch (root->data.operatorNum)
     {
@@ -87,10 +96,8 @@ int convertToAssemblyCode(Evaluator *eval, Node *root, FILE *f)
 
         case ASSIGN:                return printCaseAssign(eval, root, f);
                                     
-        case ADD: case SUB:
-        case MUL: case DIV:
-        case POW: case LN:          
-        case LOGAR:
+        case ADD: case SUB: case MUL: case DIV:
+        case POW: case LN:  case LOGAR:
         case SIN: case COS:         convertToAssemblyCode(eval, root->left,  f);
                                     convertToAssemblyCode(eval, root->right, f);
                                     printTreeOperator(root->data.operatorNum, f);
@@ -98,23 +105,17 @@ int convertToAssemblyCode(Evaluator *eval, Node *root, FILE *f)
                                     fprintf(f, "\n");
                                     return EXIT_SUCCESS;
 
-        case BELOW:
-        case ABOVE:                 convertToAssemblyCode(eval, root->left,  f);
+        case BELOW: case ABOVE:     convertToAssemblyCode(eval, root->left,  f);
                                     convertToAssemblyCode(eval, root->right, f);
                                     fprintf(f, "sub\n\n");
                                     return EXIT_SUCCESS;
 
-        case IF:                    return printCaseIf(eval, root, f);
+        case IF:                    return printCaseIf   (eval, root, f);
 
-        case WHILE:
+        case WHILE:                 return printCaseWhile(eval, root, f);
 
-
-
-
-        case OPEN_F:
-        case CLOSE_F:
-        case R_BRACKET:
-        case L_BRACKET:
+        case OPEN_F:    case CLOSE_F:
+        case R_BRACKET: case L_BRACKET:
         case NOT_OPER:
         default:                    LOG("ERROR in %s(%d) in function %s: unsupported operator: %d\n",
                                         __FILE__, __LINE__ - 1, __func__, root->data.operatorNum);
@@ -173,34 +174,77 @@ int printCaseIf(Evaluator *eval, Node *root, FILE *f)
     static int ifStaticNumber = 0;
     int ifNumber = ifStaticNumber + 1;
 
+    const char *prefix = "end_if_";
+
     convertToAssemblyCode(eval, root->left,  f);
-    fprintf(f, "\npush 0\n");
+    fprintf(f, "push 0\n");
 
     Node *node = root->left;
 
     if (node->type == EXP_TREE_OPERATOR)
     {
-        int oper = node->data.operatorNum;
-
-        switch (oper)
-        {
-            case ABOVE:     fprintf(f, "jbe :end_if_%d\n\n", ifNumber);
-                            break;
-
-            case BELOW:     fprintf(f, "jae :end_if_%d\n\n", ifNumber);
-                            break;
-
-            default:        fprintf(f, "jne :end_if_%d\n\n", ifNumber);
-                            break;
-        }
+         printJmpOperator(node->data.operatorNum, prefix, ifNumber, f);
     }
-    else                    fprintf(f, "jne :end_if_%d\n\n", ifNumber);
+    else fprintf(f, "jne :%s%d\n\n", prefix, ifNumber);
     
     ifStaticNumber++;
     convertToAssemblyCode(eval, root->right,  f);
-    fprintf(f, ":end_if_%d\n\n", ifNumber);
+    fprintf(f, ":%s%d\n\n", prefix, ifNumber);
     ifNumber++;
 
     return EXIT_SUCCESS;
 }
 
+int printCaseWhile(Evaluator *eval, Node *root, FILE *f)
+{
+    assert(eval);
+    assert(root);
+    assert(f);
+
+    static int whileStaticNumber = 0;
+    int whileNumber = whileStaticNumber + 1;
+
+    const char *prefixBegin = "while_";
+    const char *prefixEnd   = "end_while_";
+
+    fprintf(f, ":%s%d\n\n", prefixBegin, whileNumber);
+
+    convertToAssemblyCode(eval, root->left,  f);
+    fprintf(f, "push 0\n");
+
+    Node *node = root->left;
+
+    if (node->type == EXP_TREE_OPERATOR)
+    {
+         printJmpOperator(node->data.operatorNum, prefixEnd, whileNumber, f);
+    }
+    else fprintf(f, "jne :%s%d\n\n", prefixEnd, whileNumber);
+    
+    whileStaticNumber++;
+    convertToAssemblyCode(eval, root->right,  f);
+    fprintf(f, "jmp :%s%d\n",   prefixBegin, whileNumber);
+    fprintf(f, ":%s%d\n\n", prefixEnd,   whileNumber);
+    whileNumber++;
+
+    return EXIT_SUCCESS;
+}
+
+int printJmpOperator(int oper, const char *labelPrefix, int labelNum,  FILE *f)
+{
+    assert(labelPrefix);
+    assert(labelNum);
+
+    switch (oper)
+    {
+        case ABOVE:     fprintf(f, "jbe :%s%d\n\n", labelPrefix, labelNum);
+                        break;
+
+        case BELOW:     fprintf(f, "jae :%s%d\n\n", labelPrefix, labelNum);
+                        break;
+
+        default:        fprintf(f, "jne :%s%d\n\n", labelPrefix, labelNum);
+                        break;
+    }
+
+    return EXIT_SUCCESS;
+}
--- source/assembler_code.h
+++ source/assembler_code.h
@@ -7,8 +7,13 @@ int convertToAssemblyCode(Evaluator *eval, Node *root, FILE *f);
 
 int printAssemblyRegister(Evaluator *eval, Node *node, FILE *f);
 
+int printAssemblyOperator(Evaluator *eval, Node *root, FILE *f);
+
 int printCaseAssign(Evaluator *eval, Node *root, FILE *f);
 int printCaseIf    (Evaluator *eval, Node *root, FILE *f);
+int printCaseWhile (Evaluator *eval, Node *root, FILE *f);
+
+int printJmpOperator(int oper, const char *labelPrefix, int labelNum,  FILE *f);
 
 char *getFileName(const char *fileInName, const char *postfix);
 
